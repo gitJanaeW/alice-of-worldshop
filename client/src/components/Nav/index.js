9129b6403ce72cc7b1b09c7718b59f1fbce4ab29
gitJanaeW <janae.wel@gmail.com>
2023-01-26 03:50:54 -0500

    navbar css done

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,16 @@
+import logo from "../../../public/logo.jpg";
+
 export default function Nav() {
     return(
-        <>
-            
-        </>
+        <nav>
+            <div className="beside">
+                <img className="logo" src={logo} alt="a simplistic, white shopping cart on a pink background"/>
+                <h1>ALICE OF SHOPWORLD</h1>
+            </div>
+            <ul>
+                <li>Products</li>
+                <li>My Cart</li>
+            </ul>
+        </nav>
     );
 };
