7afd170ea7daecb10a56b0308a799808dc99ad28
gitJanaeW <janae.wel@gmail.com>
2023-01-26 01:34:26 -0500

    react app built successfully

---
+++ webpack.config.cjs
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  target: 'web',
+  devServer: {
+    static: path.resolve(__dirname, 'client/dist'),
+    port: 3000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        include: path.resolve(__dirname, 'client/src'),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        },
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "client/src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|pdf)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", "jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "client/public", "index.html"),
+    }),
+  ]
+}
