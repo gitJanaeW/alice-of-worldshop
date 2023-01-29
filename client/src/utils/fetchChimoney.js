fd673ed1c1ac27c8915097bcbed2e38f8f615459
gitJanaeW <janae.wel@gmail.com>
2023-01-28 19:56:02 -0500

    fetch data displayed

--- client/src/utils/fetchChimoney.js
+++ client/src/utils/fetchChimoney.js
@@ -1,13 +1,25 @@
-const returnChimoney = () => {
-    const sdk = require("api")('@chimoney/v0.2.1#2w2jenlbc6ubix');
-    sdk.auth("5fa47041cf1bca32b11f72a3bac177bcbec210479c06821401b5e3501ca7e262");
+let assets = null;
 
-    sdk.getV02InfoAssets()
-    .then(({data}) => {
-        console.log(data)
-    })
-    .catch(err => console.error(err));
+// const fetchChimoney = async () => {
+//     const sdk = require("api")('@chimoney/v0.2.1#2w2jenlbc6ubix');
+//     sdk.auth("5fa47041cf1bca32b11f72a3bac177bcbec210479c06821401b5e3501ca7e262");
+//     try {
+//         assets = await sdk.getV02InfoAssets();
+//     } catch (err) {
+//         console.log(err);
+//     }
+//     return assets;
+// };
+
+
+const fetchStore = () => {
+    fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json=> {assets = json; console.log(assets[0].title)});
+    return assets;
 }
 
+fetchStore();
 
-
+// module.exports = fetchChimoney;
+module.exports = fetchStore;
---
+++ client/src/utils/test.js
@@ -0,0 +1,3 @@
+const assets = require("./fetchChimoney.js");
+
+console.log(assets);
